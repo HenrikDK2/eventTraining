4e9ed78fc8b12e68c8bf4bbc68ffeea91a473a7a
Henrik Mundt Milo <4519362@RTS-365.dk>
2019-09-13 09:48:33 +0200

    Javascript

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(e){
+    const ul = document.querySelector('ul');
+
+    fetch('./assets/json/data.json').then(response => {
+        return response.json();
+    }).then(data =>{
+        data.event.forEach(element => {
+            ul.innerHTML += `
+                <li>
+                    <h2>${element.target+': '+element.result}</h2>
+                    <h4>Hvordan: ${element.eventTarget}<h4>
+                </li>`;
+                
+        });
+    });
+});
